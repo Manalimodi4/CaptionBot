b924986a40be88a636a39c3b200c7a369ffebb87
Ajinkya Bawaskar <ajinkyabawaskar2@gmail.com>
2020-03-18 17:39:00 +0530

    wip

--- static/main.js
+++ static/main.js
@@ -12,20 +12,21 @@ fileDrag.addEventListener("drop", fileSelectHandler, false);
 fileSelect.addEventListener("change", fileSelectHandler, false);
 
 function fileDragHover(e) {
-  // prevent default behaviour
-  e.preventDefault();
-  e.stopPropagation();
+    // prevent default behaviour
+    e.preventDefault();
+    e.stopPropagation();
 
-  fileDrag.className = e.type === "dragover" ? "upload-box dragover" : "upload-box";
+    fileDrag.className = e.type === "dragover" ? "upload-box dragover" : "upload-box";
 }
 
 function fileSelectHandler(e) {
-  // handle file selecting
-  var files = e.target.files || e.dataTransfer.files;
-  fileDragHover(e);
-  for (var i = 0, f; (f = files[i]); i++) {
-    previewFile(f);
-  }
+    // handle file selecting
+    var files = e.target.files || e.dataTransfer.files;
+    fileDragHover(e);
+    for (var i = 0, f;
+        (f = files[i]); i++) {
+        previewFile(f);
+    }
 }
 
 //========================================================================
@@ -43,58 +44,58 @@ var loader = document.getElementById("loader");
 //========================================================================
 
 function submitImage() {
-  // action for the submit button
-  console.log("submit");
+    // action for the submit button
+    console.log("submit");
 
-  if (!imageDisplay.src || !imageDisplay.src.startsWith("data")) {
-    window.alert("Please select an image before submit.");
-    return;
-  }
+    if (!imageDisplay.src || !imageDisplay.src.startsWith("data")) {
+        window.alert("Please select an image before submit.");
+        return;
+    }
 
-  loader.classList.remove("hidden");
-  imageDisplay.classList.add("loading");
+    loader.classList.remove("hidden");
+    imageDisplay.classList.add("loading");
 
-  // call the predict function of the backend
-  predictImage(imageDisplay.src);
+    // call the predict function of the backend
+    predictImage(imageDisplay.src);
 }
 
 function clearImage() {
-  // reset selected files
-  fileSelect.value = "";
+    // reset selected files
+    fileSelect.value = "";
 
-  // remove image sources and hide them
-  imagePreview.src = "";
-  imageDisplay.src = "";
-  predResult.innerHTML = "";
+    // remove image sources and hide them
+    imagePreview.src = "";
+    imageDisplay.src = "";
+    predResult.innerHTML = "";
 
-  hide(imagePreview);
-  hide(imageDisplay);
-  hide(loader);
-  hide(predResult);
-  show(uploadCaption);
+    hide(imagePreview);
+    hide(imageDisplay);
+    hide(loader);
+    hide(predResult);
+    show(uploadCaption);
 
-  imageDisplay.classList.remove("loading");
+    imageDisplay.classList.remove("loading");
 }
 
 function previewFile(file) {
-  // show the preview of the image
-  console.log(file.name);
-  var fileName = encodeURI(file.name);
+    // show the preview of the image
+    console.log(file.name);
+    var fileName = encodeURI(file.name);
 
-  var reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onloadend = () => {
-    imagePreview.src = URL.createObjectURL(file);
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+        imagePreview.src = URL.createObjectURL(file);
 
-    show(imagePreview);
-    hide(uploadCaption);
+        show(imagePreview);
+        hide(uploadCaption);
 
-    // reset
-    predResult.innerHTML = "";
-    imageDisplay.classList.remove("loading");
+        // reset
+        predResult.innerHTML = "";
+        imageDisplay.classList.remove("loading");
 
-    displayImage(reader.result, "image-display");
-  };
+        displayImage(reader.result, "image-display");
+    };
 }
 
 //========================================================================
@@ -102,46 +103,46 @@ function previewFile(file) {
 //========================================================================
 
 function predictImage(image) {
-  fetch("/predict", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(image)
-  })
-    .then(resp => {
-      if (resp.ok)
-        resp.json().then(data => {
-          displayResult(data);
+    fetch("/predict", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(image)
+        })
+        .then(resp => {
+            if (resp.ok)
+                resp.json().then(data => {
+                    displayResult(data);
+                });
+        })
+        .catch(err => {
+            console.log("An error occured", err.message);
+            window.alert("Oops! Something went wrong.");
         });
-    })
-    .catch(err => {
-      console.log("An error occured", err.message);
-      window.alert("Oops! Something went wrong.");
-    });
 }
 
 function displayImage(image, id) {
-  // display image on given id <img> element
-  let display = document.getElementById(id);
-  display.src = image;
-  show(display);
+    // display image on given id <img> element
+    let display = document.getElementById(id);
+    display.src = image;
+    show(display);
 }
 
 function displayResult(data) {
-  // display the result
-  // imageDisplay.classList.remove("loading");
-  hide(loader);
-  predResult.innerHTML = data.result;
-  show(predResult);
+    // display the result
+    // imageDisplay.classList.remove("loading");
+    hide(loader);
+    predResult.innerHTML = data.result;
+    show(predResult);
 }
 
 function hide(el) {
-  // hide an element
-  el.classList.add("hidden");
+    // hide an element
+    el.classList.add("hidden");
 }
 
 function show(el) {
-  // show an element
-  el.classList.remove("hidden");
+    // show an element
+    el.classList.remove("hidden");
 }
